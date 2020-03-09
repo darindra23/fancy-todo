28337bdd222bdd325618ac8761bf1dd857f5791d
darindra23 <58590046+darindra23@users.noreply.github.com>
2020-03-09 16:30:33 +0700

    revisi

--- server/config/config.js
+++ server/config/config.js
@@ -5,5 +5,12 @@ module.exports = {
     "database": "todos",
     "host": "127.0.0.1",
     "dialect": "postgres"
+  },
+  "production": {
+    "username": "uhsxejheylokcb",
+    "password": "cbd0819e8380e0b5c9db546351cad95bb3f1ba5a3c681ee569aa4e725e961929",
+    "database": "dj004a935is05",
+    "host": "ec2-18-210-51-239.compute-1.amazonaws.com",
+    "dialect": "postgres"
   }
 }
